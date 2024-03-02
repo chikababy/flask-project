455b7811195088908c38bd04a123d881dcaf8f1c
chikababy <154565496+chikababy@users.noreply.github.com>
2024-03-02 17:33:26 -0500

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent { label 'ansible-master' }
+
+    stages {
+        stage('Deploy with Playbook') {
+            steps {
+                // Execute Ansible playbook for deployment
+                sh '/usr/bin/ansible-playbook -i /home/centos/flask-project/hosts.ini /home/centos/flask-project/01-install-flask.yml'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Flask app deployment successful!'
+        }
+        failure {
+            echo 'Flask app deployment failed!' 
+        }
+    }
+}
